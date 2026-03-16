f30a993f18ca3c71593d4e0906e9885cc51f8da9
Neo Zhen Ting <neozt99@gmail.com>
2026-03-16 18:20:56 +0800

    feat: add endpoint to list pending claims

---
+++ functions/handlers/list-pending-claims.mjs
@@ -0,0 +1,41 @@
+import {DynamoDBClient, ScanCommand} from "@aws-sdk/client-dynamodb";
+
+const dynamoClient = new DynamoDBClient({});
+
+export const handler = async (event) => {
+    console.log("Received event:", JSON.stringify(event));
+
+    try {
+        const tableName = process.env.DYNAMODB_TABLE_NAME;
+        const scanItemCommand = new ScanCommand({
+            TableName: tableName,
+            FilterExpression: "#status = :status",
+            ExpressionAttributeNames: {
+                '#status': 'status'
+            },
+            ExpressionAttributeValues: {
+                ':status': {
+                    S: "PENDING_APPROVE"
+                }
+            },
+        });
+
+        const result = await dynamoClient.send(scanItemCommand);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+              data: result?.Items
+            })
+        };
+    } catch (error) {
+        console.error("Error when listing pending claims: ", error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Something went wrong. Please try again later.",
+            })
+        };
+    }
+};
